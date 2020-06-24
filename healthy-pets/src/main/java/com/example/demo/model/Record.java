6ca4599b0e689aa9070a6def466702e36cd4dc9e
Yimin596 <66429069+Yimin596@users.noreply.github.com>
2020-06-23 22:58:50 -0500

    Add files via upload

--- healthy-pets/src/main/java/com/example/demo/model/Record.java
+++ healthy-pets/src/main/java/com/example/demo/model/Record.java
@@ -2,6 +2,9 @@ package com.example.demo.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
@@ -9,7 +12,64 @@ import javax.validation.constraints.NotBlank;
 @Table(name = "records")
 public class Record {
 
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+	
 	@NotBlank(message = "Name is mandatory")
     @Column(name = "name")
     private String name;
+	
+	
+	@NotBlank(message = "Date is mandatory")
+	@Column(name = "Date")
+	private Date date;
+	
+	@NotBlank(message = "Event is mandatory")
+	@Column(name = "Event")
+	private Event event;
+	
+	public Record() {}
+	
+	public Record(String pet, Date date, Event event)
+	{
+		this.name = pet;
+		this.date = date;
+		this.event = event;
+		
+	}
+	
+	public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+	public void setPetName(String petname)
+	{
+		name = petname;
+	}
+	public String getPetName()
+	{
+		return name;
+	}
+	public void setDate(Date date)
+	{
+		this.date = date;
+	}
+	public Date getDate()
+	{
+		return date;
+	}
+	public void setEvent(Event event)
+	{
+		this.event = event;
+	}
+	public Event getEvent()
+	{
+		return event;
+	}
 }
